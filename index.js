c943d7ce97a23d596b642c93dc763b0b4fdc27f2
Tanesya <tanesya.t@gmail.com>
2024-01-14 22:19:21 +0700

    chore: generate card from book list

--- index.js
+++ index.js
@@ -19,13 +19,30 @@ async function getData(url) {
   const { data } = await response.json();
 
   data.forEach((item) => {
+    const cardElement = document.createElement("div");
+    cardElement.className = "card";
+    imageElement = document.createElement("img");
+    titleElement = document.createElement("h5");
+    authorElement = document.createElement("p");
+
     item.authors = parseColSingle(item.authors);
     item.genre = parseColSingle(item.genre);
     item.categories = parseColSingle(item.categories);
     item.also_enjoy = item.also_enjoy.split("], ").forEach((splitted) => {
-       parseColMulti(splitted)
+      parseColMulti(splitted)
     })
+    
+    imageElement.src = item.thumbnail;
+    titleElement.textContent = item.title;
+    authorElement.textContent = item.authors;
+
+    console.log(item);
+    cardElement.append(imageElement, titleElement, authorElement);
+    document.body.append(cardElement);
   });
 }
 
+
+
 getData("https://v1.appbackend.io/v1/rows/kSezugEdycRP");
+
